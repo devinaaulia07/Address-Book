e122e10863b06caa513cfb5a3e8dcdd9cc714ffd
devinaaulia07 <devinaa0702@gmail.com>
2025-11-20 17:50:54 +0700

    Update

---
+++ scripts/scripts.js
@@ -0,0 +1,34 @@
+const contacts = [
+  {
+    id: 1,
+    fullName: "Devina Aulia",
+    phone: 6282320747396,
+    email: "devinaa07@gmail.com",
+  },
+  {
+    id: 2,
+    fullName: "nadila",
+    phone: 6289700654321,
+    email: "nadila99@gmail.com",
+  },
+  {
+     id: 3,
+    fullName: "kaylla",
+    phone: 6289733678955,
+    email: "kayllaaa@gmail.com",
+  },
+  { 
+    id: 4,
+    fullName: "rendy",
+    phone: 6282224544321,
+    email: "rednyyy84@gmail.com",
+  }  
+  {
+     id: 5,
+    fullName: "firgi",
+    phone: 6289888322321,
+    email: "firgii02@gmail.com",
+  }
+];
+
+console.log(contacts);
